a560fc1c8def8d63c00e91fb9a1fbe3356452bf9
chenlehui <chenlehui@example.com>
2021-10-22 19:16:30 +0800

    增加demo

--- example/main.go
+++ example/main.go
@@ -1,9 +1,3 @@
-# redis-util
-方便业务使用的redis操作封装，比如常见的缓存set get操作， 一行代码搞定,不像开源库需要写好多行
-
-# 使用方法
-
-```
 package main
 
 import (
@@ -50,6 +44,3 @@ func main() {
 	value, _ = redisUtil.Decr(cacheKey)
 	fmt.Println("Decr:", value)
 }
-
-```
-
